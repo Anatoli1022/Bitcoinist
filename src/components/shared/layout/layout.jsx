de0e4e85f879d4a0d302de56dc87d7ebad41621e
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-02-23 17:11:50 +0300

    feat: adds section Header to the component Layout

--- src/components/shared/layout/layout.jsx
+++ src/components/shared/layout/layout.jsx
@@ -4,10 +4,13 @@ import React from 'react';
 
 import styles from './layout.module.scss';
 
+import Header from 'components/shared/header';
+
 const cx = classNames.bind(styles);
 
 const Layout = ({ children }) => (
   <div className={cx('wrapper')}>
+    <Header />
     <main className={cx('content')}>{children}</main>
   </div>
 );
