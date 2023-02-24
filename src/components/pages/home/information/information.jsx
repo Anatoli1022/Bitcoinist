d0030608d70fb1d09e229f21290ebbbc6049fa9b
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-02-24 10:43:52 +0300

    feat: adds section information

---
+++ src/components/pages/home/information/information.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+
+import styles from './information.module.scss';
+
+import advanced from './images/information-advanced.svg';
+import item from './images/information-item.svg';
+
+const cx = classNames.bind(styles);
+
+const Information = () => (
+  <section className={cx('information')}>
+    <div className={cx('container-xl')}>
+      <div className={cx('information-one')}>
+        <div>
+          <h2 className={cx('information-title')}>Easy to use, advanced Estimates editor</h2>
+          <p className={cx('information-text')}>
+            Our easy to use, advenced editor will help you create estimates faster, confident and
+            without any errors.
+          </p>
+        </div>
+        <div className={cx('information-image')}>
+          <img src={advanced} alt="" loading="lazy" aria-hidden="true" />
+        </div>
+      </div>
+      <div className={cx('information-two')}>
+        <div className={cx('information-info')}>
+          <h2 className={cx('information-title')}>Hourly rates, item and services lists</h2>
+          <p className={cx('information-text')}>
+            Our easy to use, advenced editor will help you create estimates faster, confident and
+            without any errors.
+          </p>
+        </div>
+        <div className={cx('information-wrapper')}>
+          <img src={item} alt="" loading="lazy" aria-hidden="true" />
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+export default Information;
