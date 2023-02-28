7af0b75fa53a894d493e78ebfea045ff4a3ecbc2
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-02-28 18:20:00 +0300

    feat: adds mobile version templates

--- src/components/pages/home/templates/templates.jsx
+++ src/components/pages/home/templates/templates.jsx
@@ -9,32 +9,32 @@ import templates from './images/templates-templates.svg';
 const cx = classNames.bind(styles);
 
 const Templates = () => (
-  <section className={cx('information')}>
+  <section className={cx('templates')}>
     <div className={cx('container-xl')}>
-      <div className={cx('information-one')}>
+      <div className={cx('templates-one')}>
         <div>
-          <h2 className={cx('information-title')}>Get Business Performance Insights</h2>
-          <p className={cx('information-text')}>
+          <h2 className={cx('templates-title')}>Get Business Performance Insights</h2>
+          <p className={cx('templates-text')}>
             Keep track of how many estimates you have created, and how much have been approved by
             client.
             <br /> You can also compare your performances from month to month, quarter to quarter,
             or annual to annual.
           </p>
         </div>
-        <div className={cx('information-image')}>
-          <img src={business} alt="" loading="lazy" aria-hidden="true" />
+        <div className={cx('wrapper-image')}>
+          <img src={business} alt="" loading="lazy" aria-hidden="true" className={cx('image')} />
         </div>
       </div>
-      <div className={cx('information-two')}>
-        <div className={cx('information-info')}>
-          <h2 className={cx('information-title')}>Estimate Templates</h2>
-          <p className={cx('information-text')}>
+      <div className={cx('templates-two')}>
+        <div className={cx('templates-info')}>
+          <h2 className={cx('templates-title')}>Estimate Templates</h2>
+          <p className={cx('templates-text')}>
             Selection of professionally designed templates. Upload your logo, select matching color,
             and you are ready to go.
           </p>
         </div>
-        <div className={cx('information-wrapper')}>
-          <img src={templates} alt="" loading="lazy" aria-hidden="true" />
+        <div className={cx('container-image')}>
+          <img src={templates} alt="" loading="lazy" aria-hidden="true" className={cx('image')} />
         </div>
       </div>
     </div>
